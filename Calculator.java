c17c86e38253035fa23d1218261658e31c1285f5
DUDEamee <ameenameen8701@gmail.com>
2025-02-03 17:53:32 +0530

    update : JFrame right,color changed to white

--- Calculator.java
+++ Calculator.java
@@ -1,5 +1,7 @@
+import java.awt.Color;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 
 public class Calculator {
@@ -9,13 +11,23 @@ public class Calculator {
         jf.setSize(600, 600);
         jf.setLocation(300, 150);
 
-        JLabel displayLabel=new JLabel("hello world");
+        JLabel displayLabel=new JLabel("hello this is a calculator");
         displayLabel.setBounds(30, 50, 540, 40);
-
         jf.add(displayLabel);
+        displayLabel.setHorizontalAlignment(SwingConstants.RIGHT);
+        displayLabel.setForeground(Color.white);
+        displayLabel.setOpaque(true);
+        displayLabel.setBackground(Color.GRAY);
+
+
+        
+
+
 
         jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         jf.setVisible(true);
+
+
     }
 
     public static void main(String[] args) {
